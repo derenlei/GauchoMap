4174bf515bbe6b73a9439f957115fb07f6ae15bf
melvinb9@gmail.com <mnguyen00@umail.ucsb.edu>
2017-06-06 11:11:06 -0700

    drawer complete. TODO: implement fragments

--- app/src/main/java/edu/ucsb/cs/cs190i/derenlei/GauchoMap/MapsActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs190i/derenlei/GauchoMap/MapsActivity.java
@@ -17,10 +17,20 @@ import com.google.android.gms.location.LocationListener;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -53,11 +63,12 @@ import static edu.ucsb.cs.cs190i.derenlei.GauchoMap.R.id.map;
 import static edu.ucsb.cs.cs190i.derenlei.GauchoMap.R.id.title;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,
         GoogleMap.OnMapLoadedCallback,
         GoogleApiClient.OnConnectionFailedListener,
         GoogleApiClient.ConnectionCallbacks,
         LocationListener,GoogleMap.OnMarkerClickListener,
+        NavigationView.OnNavigationItemSelectedListener,
         GoogleMap.OnInfoWindowClickListener, ResultCallback<Status> {
 
     public static boolean loggedIn = false;
@@ -83,19 +94,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(!loggedIn) {
-            loggedIn = true;
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
-        }
+//        if(!loggedIn) {
+//            loggedIn = true;
+//            Intent intent = new Intent(this, LoginActivity.class);
+//            startActivity(intent);
+//        }
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setContentView(R.layout.activity_maps);
+//
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
 
-        setContentView(R.layout.activity_maps);
         locations = new ArrayList<>();
         //webServerController = new WebServerController();
         //WebServerController.retrieveLocations(this);
-        SupportMapFragment mf = (SupportMapFragment) getSupportFragmentManager().findFragmentById(map);
-        mf.getMapAsync(this);
+//        SupportMapFragment mf = (SupportMapFragment) getSupportFragmentManager().findFragmentById(map);
+//        mf.getMapAsync(this);
     }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+//        if (!inListView && id == R.id.list_view) {
+//            inListView = true;
+//
+//            if(listFrag==null) {
+//                listFrag = new ListFragment();
+//            }
+//            getFragmentManager().beginTransaction().replace(R.id.fragment_container, listFrag).commit();
+//            //Log.d("LIST","going to list view");
+//        }
+//        else if (inListView && id == R.id.grid_view) {
+//            inListView = false;
+//
+//            if(cFragment==null){
+//                cFragment = new CalendarFragment();
+//            }
+//            getFragmentManager().beginTransaction().replace(R.id.fragment_container, cFragment).commit();
+//            TextView upload = (TextView)findViewById(R.id.upload);
+//            upload.setVisibility(View.INVISIBLE);
+//            //Log.d("GRID","going to grid view");
+//        }
+//        else if (id==R.id.logout){
+//            Intent intent = new Intent(this, LoginActivity.class);
+//            this.startActivity(intent);
+//        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
 
 
     @Override
